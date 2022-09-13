c964591a71c653b2282af844b5187b01fe5ef19d
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2022-09-14 01:08:36 +0900

    Add files via upload

---
+++ chapter3/util/Adder.java
@@ -0,0 +1,10 @@
+package com.fastcampus.functionalprogramming.chapter3.util;
+
+import java.util.function.Function;
+
+public class Adder implements Function<Integer, Integer>{
+	@Override
+	public Integer apply(Integer x) { // function interface를 구현할 함수
+		return x + 10;
+	}
+}
